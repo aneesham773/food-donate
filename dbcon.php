c30ed168fe8023d157869d57ad8980c98e2d2ce2
aneesham773 <61931107+aneesham773@users.noreply.github.com>
2021-04-27 15:08:25 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,20 @@
+<?php
+$server="localhost:3307";
+$user="root";
+$password="";
+$db="signup";
+$con=mysqli_connect($server,$user,$password,$db);
+if($con){
+	?>
+	<script >
+		alert("Connection Successful");
+	</script>
+	<?php
+}else{
+	?>
+	<script >
+		alert("No Connection");
+	</script>
+	<?php
+}
+?>
