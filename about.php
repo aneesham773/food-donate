9a88faa8bfad7a4f0e219233f313d91146c120bd
aneesham773 <61931107+aneesham773@users.noreply.github.com>
2021-04-27 14:58:24 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About Us</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   <script src="https://kit.fontawesome.com/yourcode.js"></script>
+    <script src="https://kit.fontawesome.com/a0941870c8.js" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+
+     <link rel="stylesheet" type="text/css" href="queries.css">
+
+	<link rel="stylesheet" type="text/css" href="about-css.css">
+</head>
+<body>
+	      
+	
+
+
+	<div class="container">
+		<center>
+			<h1>About Us</h1>
+			<div class="img">
+				
+			</div>
+			<p>
+				<h3>
+			Food Bank is a crowdfunding platform that enables donors to directly donate food products to NGOs and charities online instead of money. As a donor, you are guaranteed 100% transperancy. After you donate products, you will receive an update from the concerned organisation.
+		</h3>
+		<h1>HOW TO DONATE?</h1>
+		<h3>
+			Choose the products on our Food Bank and add them to the cart. Fill in the required details and mode of payment. Submit the details for the order to be placed successfully.
+		</h3>
+			</p>
+		</center>
+	</div>
+	
+</body>
+
+</html>
